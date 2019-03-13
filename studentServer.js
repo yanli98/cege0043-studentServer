06d954f93f7933dbcf2200b6ecd16c1ccd02c400
yanli98 <yan.li.18@ucl.ac.uk>
2019-03-13 16:14:04 -0400

    Import the required database connectivity code and set up a database connection

--- studentServer.js
+++ studentServer.js
@@ -16,6 +16,20 @@ extended: true
 }));
 app.use(bodyParser.json());
 
+var fs = require('fs');
+var pg = require('pg');
+var configtext =
+""+fs.readFileSync("/home/studentuser/certs/postGISConnection.js");
+// now convert the configruation file into the correct format -i.e. a name/value
+pair array
+var configarray = configtext.split(",");
+var config = {};
+for (var i = 0; i < configarray.length; i++) {
+var split = configarray[i].split(':');
+config[split[0].trim()] = split[1].trim();
+}
+var pool = new pg.Pool(config);
+
 
 // adding functionality to log the requests
 app.use(function (req, res, next) {
