dec549e3a4950486bfc33b2d64b1c96146ba1cec
yanli98 <yan.li.18@ucl.ac.uk>
2019-03-13 13:53:58 -0400

    use express static routes to access to any html file that the user requests

--- studentServer.js
+++ studentServer.js
@@ -9,15 +9,6 @@ var http = require('http');
 var httpServer = http.createServer(app);
 httpServer.listen(4480);
 
-// adding GET Functionality
-app.get('/:fileName', function (req, res) {
-// run some server-side code
-var fileName = req.params.fileName;
-console.log(fileName + ' requested');
-// note that __dirname gives the path to the studentServer.js file
-res.sendFile(__dirname + '/'+ fileName);
-});
-
 // adding functionality to log the requests
 app.use(function (req, res, next) {
 var filename = path.basename(req.url);
@@ -26,3 +17,6 @@ console.log("The file " + filename + " was requested.");
 next();
 });
 
+// serve static files - e.g. html, css
+// this should always be the last line in the server file
+app.use(express.static(__dirname));
